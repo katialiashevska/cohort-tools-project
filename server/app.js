3eb1c56607f625a7df8c349c99e2968fc5f59db1
Katia <kate.liashevska@gmail.com>
2023-08-15 17:59:41 +0200

    set up mongoose & models

--- server/app.js
+++ server/app.js
@@ -1,46 +1,55 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const PORT = 5005;
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
+const express = require("express")
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const PORT = 5005
+const cohorts = require("./cohorts.json")
+const students = require("./students.json")
+const mongoose = require("mongoose")
+
+const Cohort = require("./models/Cohort.model")
+const Student = require("./models/Student.model")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
-const app = express();
+const app = express()
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
 app.use(
-  cors({
-    origin: ["http://127.0.0.1:5173"],
-  })
-);
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+    cors({
+        origin: ["http://127.0.0.1:5173"],
+    })
+)
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+    .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+    .catch(err => console.error("Error connecting to MongoDB", err))
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
-});
+    res.sendFile(__dirname + "/views/docs.html")
+})
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
-});
+    res.json(cohorts)
+})
 app.get("/api/students", (req, res) => {
-  res.json(students);
-});
+    res.json(students)
+})
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+    console.log(`Server listening on port ${PORT}`)
+})
