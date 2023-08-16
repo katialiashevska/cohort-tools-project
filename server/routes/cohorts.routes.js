bc1a8f6846078f63262d068f4d650bac1aca33d3
agathelurquin <agathe.lurquin@essca.eu>
2023-08-16 17:19:04 +0200

    added static route for students

--- server/routes/cohorts.routes.js
+++ server/routes/cohorts.routes.js
@@ -1,67 +1,67 @@
-const router = require("express").Router()
-const Cohort = require("./../models/Cohort.model")
+const router = require("express").Router();
+const Cohort = require("./../models/Cohort.model");
 
-router.get("/", async (req, res, next) => {
-    try {
-        const allCohorts = await Cohort.find()
-        res.json(allCohorts)
-    } catch (error) {
-        console.log(error)
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const allCohorts = await Cohort.find();
+    res.json(allCohorts);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.post("/", (req, res) => {
-    Cohort.create({
-        cohortSlug: req.body.cohortSlug,
-        cohortName: req.body.cohortName,
-        program: req.body.program,
-        format: req.body.format,
-        campus: req.body.campus,
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
-        programManager: req.body.programManager,
-        leadTeacher: req.body.leadTeacher,
-        totalHours: req.body.totalHours,
+  Cohort.create({
+    cohortSlug: req.body.cohortSlug,
+    cohortName: req.body.cohortName,
+    program: req.body.program,
+    format: req.body.format,
+    campus: req.body.campus,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    programManager: req.body.programManager,
+    leadTeacher: req.body.leadTeacher,
+    totalHours: req.body.totalHours,
+  })
+    .then((createdCohort) => {
+      res.status(201).json(createdCohort);
     })
-        .then(createdCohort => {
-            res.status(201).json(createdCohort)
-        })
-        .catch(error => {
-            res.status(500).json({ error: "Failed to create a cohort" })
-        })
-})
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to create a cohort" });
+    });
+});
 
 router.get("/:cohortId", (req, res) => {
-    const cohortId = req.params.cohortId
-    Cohort.findById(cohortId)
-        .then(oneCohort => {
-            res.status(200).json(oneCohort)
-        })
-        .catch(error => {
-            res.status(500).json({ error: "Failed to get a single cohort" })
-        })
-})
+  const cohortId = req.params.cohortId;
+  Cohort.findById(cohortId)
+    .then((oneCohort) => {
+      res.status(200).json(oneCohort);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to get a single cohort" });
+    });
+});
 
 router.put("/:cohortId", (req, res) => {
-    const cohortId = req.params.cohortId
-    Cohort.findByIdAndUpdate(cohortId, req.body, { new: true })
-        .then(updatedCohort => {
-            res.status(200).json(updatedCohort)
-        })
-        .catch(error => {
-            res.status(500).json({ error: "Failed to update a single cohort" })
-        })
-})
+  const cohortId = req.params.cohortId;
+  Cohort.findByIdAndUpdate(cohortId, req.body, { new: true })
+    .then((updatedCohort) => {
+      res.status(200).json(updatedCohort);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to update a single cohort" });
+    });
+});
 
 router.delete("/:cohortId", (req, res) => {
-    const cohortId = req.params.cohortId
-    Cohort.findByIdAndDelete(cohortId)
-        .then(() => {
-            res.status(200).send()
-        })
-        .catch(error => {
-            res.status(500).json({ error: "Failed to delete a single cohort" })
-        })
-})
+  const cohortId = req.params.cohortId;
+  Cohort.findByIdAndDelete(cohortId)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to delete a single cohort" });
+    });
+});
 
-module.exports = router
+module.exports = router;
--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -1,40 +1,39 @@
-const router = require("express").Router()
-const Student = require("./../models/Student.model")
+const router = require("express").Router();
+const Student = require("./../models/Student.model");
+const students = require("./students.json");
 
 router.get("/", async (req, res) => {
-    try {
-        const allStudents = await Student.find()
-        res.json(allStudents)
-    } catch (error) {
-        console.log(error)
-    }
-})
+  try {
+    const allStudents = await Student.find();
+    res.json(allStudents);
+  } catch (error) {
+    console.log(error);
+    app.get("/", (req, res) => {
+      res.json(students);
+    });
+  }
+});
 
 router.post("/", (req, res) => {
-    Student.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        phone: req.body.phone,
-        linkedinUrl: req.body.linkedinUrl,
-        languages: req.body.languages,
-        program: req.body.program,
-        background: req.body.background,
-        image: req.body.image,
-        cohort: req.body.cohort,
-        projects: req.body.projects,
+  Student.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    linkedinUrl: req.body.linkedinUrl,
+    languages: req.body.languages,
+    program: req.body.program,
+    background: req.body.background,
+    image: req.body.image,
+    cohort: req.body.cohort,
+    projects: req.body.projects,
+  })
+    .then((createdStudent) => {
+      res.status(201).json(createdStudent);
     })
-        .then(createdStudent => {
-            res.status(201).json(createdStudent)
-        })
-        .catch(error => {
-            res.status(500).json({ error: "Failed to create a student" })
-        })
-})
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to create a student" });
+    });
+});
 
-router.get("/cohort/:cohortId", (req, res) => {
-    const cohortId = req.params.id
-    Student.
-})
-
-module.exports = router
+module.exports = router;
