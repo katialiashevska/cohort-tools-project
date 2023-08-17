fa17b359017c640af74d0943b34e47c6639016df
Nickdev2023 <n.michelpro1@gmail.com>
2023-08-17 16:26:48 +0200

    test

--- server/app.js
+++ server/app.js
@@ -1,46 +1,49 @@
-require("./config/db")
-const express = require("express")
-const morgan = require("morgan")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-const PORT = 5005
-const { errorHandler, notFoundHandler } = require("./middleware/error-handling")
+require("./config/db");
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const PORT = 5005;
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middleware/error-handling");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-const cohorts = require("./cohorts.json")
-const students = require("./students.json")
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
-const app = express()
+const app = express();
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
 app.use(
-    cors({
-        origin: ["http://localhost:5173"],
-    })
-)
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+  cors({
+    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+  })
+);
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
-    res.sendFile(__dirname + "/views/docs.html")
-})
+  res.sendFile(__dirname + "/views/docs.html");
+});
 
-app.use("/api", require("./routes/index.routes"))
+app.use("/api", require("./routes/index.routes"));
 
-app.use(errorHandler)
-app.use(notFoundHandler)
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 // START SERVER
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -1,84 +1,84 @@
-const router = require("express").Router()
-const Student = require("./../models/Student.model")
-const students = require("../students.json")
-const getQuery = require("./../utils/index")
+const router = require("express").Router();
+const Student = require("./../models/Student.model");
+const students = require("../students.json");
+const getQuery = require("./../utils/index");
 
 router.get("/", async (req, res, next) => {
-    try {
-        const allStudents = await Student.find().populate("cohort")
-        res.json(allStudents)
-    } catch (error) {
-        next(error)
-    }
-})
+  try {
+    const allStudents = await Student.find().populate("cohort");
+    res.json(allStudents);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post("/", (req, res, next) => {
-    Student.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        phone: req.body.phone,
-        linkedinUrl: req.body.linkedinUrl,
-        languages: req.body.languages,
-        program: req.body.program,
-        background: req.body.background,
-        image: req.body.image,
-        cohort: req.body.cohort,
-        projects: req.body.projects,
+  Student.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    linkedinUrl: req.body.linkedinUrl,
+    languages: req.body.languages,
+    program: req.body.program,
+    background: req.body.background,
+    image: req.body.image,
+    cohort: req.body.cohort,
+    projects: req.body.projects,
+  })
+    .then((createdStudent) => {
+      res.status(201).json(createdStudent);
     })
-        .then(createdStudent => {
-            res.status(201).json(createdStudent)
-        })
-        .catch(error => {
-            next(error)
-        })
-})
+    .catch((error) => {
+      next(error);
+    });
+});
 
 router.get("/cohort/:cohortId", async (req, res, next) => {
-    const { cohortId } = req.query
-    const query = getQuery(cohortId)
-    try {
-        const allStudents = await Student.find(query)
-        res.json(allStudents)
-    } catch (error) {
-        next(error)
-    }
-})
+  const { cohortId } = req.query;
+  const query = getQuery(cohortId);
+  try {
+    const allStudents = await Student.find(query);
+    res.json(allStudents);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/:studentId", (req, res, next) => {
-    const studentId = req.params.studentId
+  const studentId = req.params.studentId;
 
-    Student.findById(studentId)
-        .populate("cohort")
-        .then(oneStudent => {
-            console.log("tried to populate", oneStudent)
-            res.status(200).json(oneStudent)
-        })
-        .catch(error => {
-            next(error)
-        })
-})
+  Student.findById(studentId)
+    .populate("cohort")
+    .then((oneStudent) => {
+      console.log("tried to populate", oneStudent);
+      res.status(200).json(oneStudent);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 router.put("/:studentId", (req, res, next) => {
-    const studentId = req.params.studentId
-    Student.findByIdAndUpdate(studentId, req.body, { new: true })
-        .then(updatedStudent => {
-            res.status(200).json(updatedStudent)
-        })
-        .catch(error => {
-            next(error)
-        })
-})
+  const studentId = req.params.studentId;
+  Student.findByIdAndUpdate(studentId, req.body, { new: true })
+    .then((updatedStudent) => {
+      res.status(200).json(updatedStudent);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 router.delete("/:studentId", (req, res, next) => {
-    const studentId = req.params.studentId
-    Student.findByIdAndDelete(studentId)
-        .then(() => {
-            res.status(200).send()
-        })
-        .catch(error => {
-            next(error)
-        })
-})
+  const studentId = req.params.studentId;
+  Student.findByIdAndDelete(studentId)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
-module.exports = router
+module.exports = router;
